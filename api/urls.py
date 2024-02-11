1630c405c7f222976e02e2accd5cd25642023deb
DikosAs <uraevdmitrij031@gmail.com>
2024-02-11 15:59:30 +0400

    ready to deploy

--- api/urls.py
+++ api/urls.py
@@ -6,12 +6,6 @@ app_name = 'api'
 urlpatterns = [
     path('data/contacts/', views.contacts, name='contacts'),
     path('data/items/', views.items, name='items'),
-    # path('data/data_from_index_page/', views.data_from_index_page, name='data_from_index_page'),
-
-    path('account/status/', views.status, name='status'),
-    path('account/login/', views.login_, name='login'),
-    # path('account/reg/', views.reg, name='reg'),
-    path('account/logout/', views.logout_, name='logout'),
 ]
 
 
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate, logout
 from api.models import Contacts, Items
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -49,39 +50,3 @@ def contacts(request: WSGIRequest):
         })
 
     return JsonResponse({"status": "200 OK", "contacts": contacts_})
-
-
-def status(request: WSGIRequest):
-    if not (request.method == 'GET'):
-        return JsonResponse({"status": "400 Bad request"}, status=400)
-
-    if not request.user.is_authenticated:
-        return JsonResponse({"status": "200 OK", "username": ""})
-
-    return JsonResponse({"status": "200 OK", "username": request.user.username})
-
-
-def login_(request: WSGIRequest, username: str, password: str):
-    if request.method == 'GET':
-        return JsonResponse({"status": "400 Bad request"})
-
-    if request.user.is_authenticated:
-        return JsonResponse({"status": "200 OK"})
-
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return JsonResponse({"status": "200 OK", "username": username})
-    else:
-        return JsonResponse({"status": "401 Unauthorized", "message": "Wrong username or password"})
-
-
-def logout_(request: WSGIRequest):
-    if request.method == 'GET':
-        return JsonResponse({"status": "400 Bad request"})
-
-    if not request.user.is_authenticated:
-        return JsonResponse({"status": "401 Unauthorized"})
-
-    logout(request)
-    return JsonResponse({"status": "200 OK"})
